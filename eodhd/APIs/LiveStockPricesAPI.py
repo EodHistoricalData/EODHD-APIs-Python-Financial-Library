6fb4ff0ae3744fb4b2cd8f334fad35e61250b252
U_M191W <glebivanchenko@bayestech.ru>
2023-08-08 09:20:23 +0300

    Added s parametr to get_live_stock_prices func

--- eodhd/APIs/LiveStockPricesAPI.py
+++ eodhd/APIs/LiveStockPricesAPI.py
@@ -2,13 +2,17 @@ from .BaseAPI import BaseAPI
 
 class LiveStockPricesAPI(BaseAPI):
 
-    def get_live_stock_prices(self, api_token: str, ticker: str):
+    def get_live_stock_prices(self, api_token: str, ticker: str, s:str):
         
         endpoint = 'real-time/'
+        query_string = ''
 
         if ticker.strip() == "" or ticker is None:
             raise ValueError("Ticker is empty. You need to add ticker to args")
         
+        if s is not None:
+            query_string += '&s=' + str(s)
 
-        return self._rest_get_method(api_key = api_token, endpoint = endpoint, uri = ticker)
+
+        return self._rest_get_method(api_key = api_token, endpoint = endpoint, uri = ticker, querystring=query_string)
 
