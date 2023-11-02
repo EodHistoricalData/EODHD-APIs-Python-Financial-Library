3b1fa8015d05fa33d9c94a27eaff653369816ecc
U_M191W <glebivanchenko@bayestech.ru>
2023-11-02 09:12:47 +0300

    fix websocket

--- example_websockets.py
+++ example_websockets.py
@@ -1,30 +1,24 @@
 """Websocket example"""
 
-from time import sleep
+import time
 from eodhd import WebSocketClient
 
-
-def main() -> None:
-    """Main"""
-
-    websocket = WebSocketClient(
-        # Demo API key for testing purposes
+client = WebSocketClient(
         api_key="OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX",
         endpoint="crypto",
-        symbols=["BTC-USD"]
-        # api_key="OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX", endpoint="forex", symbols=["EURUSD"]
-        # api_key="OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX", endpoint="us", symbols=["AAPL"]
-    )
-    websocket.start()
+        symbols=["BTC-USD"],
+        display_stream=False,
+        display_candle_1m=True,
+        display_candle_5m=False,
+        display_candle_1h=False,
+)
+client.start()
 
-    message_count = 0
-    while True:
-        if websocket:
-            if message_count != websocket.message_count:
-                print(websocket.message)
-                message_count = websocket.message_count
-                sleep(0.25)  # output every 1/4 second, websocket is realtime
-
-
-if __name__ == "__main__":
-    main()
+try:
+    while client.running:
+        res = client.get_data()
+        print(res)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("\nStopping due to user request.")
+    client.stop()
