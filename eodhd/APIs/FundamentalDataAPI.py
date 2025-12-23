894657ced1b1d77a83977d5de4b541ffac757940
enlavan <enlavanov@protonmail.com>
2025-12-23 20:26:05 -0300

    Minor changes

--- eodhd/APIs/FundamentalDataAPI.py
+++ eodhd/APIs/FundamentalDataAPI.py
@@ -1,12 +1,14 @@
+# APIs/FundamentalDataAPI.py
+
 from .BaseAPI import BaseAPI
+
 
 class FundamentalDataAPI(BaseAPI):
 
     def get_fundamentals_data(self, api_token: str, ticker: str):
-        
         endpoint = 'fundamentals'
 
         if ticker.strip() == "" or ticker is None:
             raise ValueError("Ticker is empty. You need to add ticker to args")
-        
-        return self._rest_get_method(api_key = api_token, endpoint = endpoint, uri = ticker)
+
+        return self._rest_get_method(api_key=api_token, endpoint=endpoint, uri=ticker)
