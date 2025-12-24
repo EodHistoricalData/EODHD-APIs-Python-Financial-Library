983a7a8faba263fb688ea66bcf97ce74d7f6fff7
enlavan <enlavanov@protonmail.com>
2025-12-24 14:04:03 -0300

    Minor changes

--- eodhd/APIs/UpcomingIPOsAPI.py
+++ eodhd/APIs/UpcomingIPOsAPI.py
@@ -1,9 +1,11 @@
+#APIs/UpcomingIPOsAPI.py
+
 from .BaseAPI import BaseAPI
 
 class UpcomingIPOsAPI(BaseAPI):
 
-    def get_upcoming_IPOs_data(self, api_token: str, from_date = None, to_date = None):
-        
+    def get_upcoming_IPOs_data(self, api_token: str, from_date=None, to_date=None):
+
         endpoint = 'calendar/ipos'
 
         query_string = ''
@@ -13,5 +15,4 @@ class UpcomingIPOsAPI(BaseAPI):
         if to_date is not None:
             query_string += '&to=' + str(to_date)
 
-        
-        return self._rest_get_method(api_key = api_token, endpoint = endpoint, querystring = query_string)
+        return self._rest_get_method(api_key=api_token, endpoint=endpoint, querystring=query_string)
