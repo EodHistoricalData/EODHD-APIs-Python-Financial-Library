ad7c5691e23c6110c5c36437d5675bb1b5e93caa
enlavan <enlavanov@protonmail.com>
2025-12-24 12:51:55 -0300

    Minor changes

--- eodhd/APIs/LiveStockPricesAPI.py
+++ eodhd/APIs/LiveStockPricesAPI.py
@@ -1,18 +1,20 @@
+#APIs/LiveStockPricesAPI.py
+
 from .BaseAPI import BaseAPI
+
 
 class LiveStockPricesAPI(BaseAPI):
 
-    def get_live_stock_prices(self, api_token: str, ticker: str, s:str):
-        
+    def get_live_stock_prices(self, api_token: str, ticker: str, s: str):
+
         endpoint = 'real-time'
         query_string = ''
 
         if ticker.strip() == "" or ticker is None:
             raise ValueError("Ticker is empty. You need to add ticker to args")
-        
+
         if s is not None:
             query_string += '&s=' + str(s)
 
-
-        return self._rest_get_method(api_key = api_token, endpoint = endpoint, uri = ticker, querystring=query_string)
+        return self._rest_get_method(api_key=api_token, endpoint=endpoint, uri=ticker, querystring=query_string)
 
